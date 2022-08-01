a5e900439bb21fa1caa2702146540e233fdbbdc6
Cléo Alves de Souza <cleonicealvesdesouza09@gmail.com>
2022-08-01 16:54:53 -0300

    Multiplos de 3 e 5

--- ex07/miniProjeto07.js
+++ ex07/miniProjeto07.js
@@ -1,10 +1,18 @@
-//Trocar os valores
-let a = 'vermelho';
-let b = 'azul';
-let aux;
+//Criar uma função somar que retorna a soma de todos os multiplos de 3 e 5
 
-aux = a;
-a = b;
-b = aux;
-console.log(a, b);
+somar(30);
+function somar(limite){
+    let multiplosde3 = 0;
+    let multiplosde5 = 0;
+    for (i = 0; i <= limite; i++){
+        if (i % 3 === 0)
+        multiplosde3 += i;
+        if (i % 5 === 0)
+        multiplosde5 += i;
+    }
+    console.log("Resultado da soma dos valores dos numeros multiplos de 3 e 5, dentro de: " + limite)
+    console.log("Multiplos de 3 " + "= " + multiplosde3);
+    console.log("Multiplos de 5 " + "= " + multiplosde5);
+    console.log("Total" + "=", multiplosde3 + multiplosde5);
+}
 
