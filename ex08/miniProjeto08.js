27f51271217b38c88ae8a5624f230daa4e55b97f
Cléo Alves de Souza <cleonicealvesdesouza09@gmail.com>
2022-08-01 18:10:06 -0300

    Média da nota escolar

--- ex08/miniProjeto08.js
+++ ex08/miniProjeto08.js
@@ -1,10 +1,23 @@
-//Trocar os valores
-let a = 'vermelho';
-let b = 'azul';
-let aux;
+// Exrecício nota escolar
+// obter a média a partir de um array
+//0-59:F
+//60-69:D
+//70-79:C
+//80-89:B
+//90-100:A
 
-aux = a;
-a = b;
-b = aux;
-console.log(a, b);
+const array = [90,50,90];
+ console.log(mediaDoAluno(array));
 
+ function mediaDoAluno(notas) {
+    let soma = 0;
+    for (let nota of notas) {
+        soma += nota
+    }
+    const media = soma/(notas.length);
+    if (media < 59) return "F";
+    if (media < 69) return "D";
+    if (media < 79) return "C";
+    if (media < 89) return "B";
+    return "A"
+ }
