f9fbc10a647538854c3f6bd4ac3e7a9982fedff0
Cléo Alves de Souza <cleonicealvesdesouza09@gmail.com>
2022-08-01 14:20:43 -0300

    FizzBuzz

--- ex03/miniProjeto03.js
+++ ex03/miniProjeto03.js
@@ -1,10 +1,23 @@
-//Trocar os valores
-let a = 'vermelho';
-let b = 'azul';
-let aux;
+//divisivel por 3 = Fizz
+//divisivel por 5 = Buzz
+//divisivel por 3 e 5 = FizzBuzz
+//não divisivel por 3 ou 5 = entrada
+//não é um numero = "não é um numero"
 
-aux = a;
-a = b;
-b = aux;
-console.log(a, b);
+const resultado = fizzbuzz("a");
+console.log(resultado);
+
+function fizzbuzz(entrada){
+    if ( typeof entrada !== "number")
+    return "não é um número";
+    if (( entrada % 3 === 0) && (entrada %5 === 0))
+    return "FizzBuzz";
+    if ( entrada % 3 === 0)
+    return "Fizz";
+    if ( entrada % 5 === 0)
+    return "Buzz";
+    
+    return entrada;
+
+}
 
