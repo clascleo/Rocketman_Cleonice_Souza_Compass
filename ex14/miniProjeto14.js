04123260055764cde26f9c5f070149a3aece78c7
Cléo Alves de Souza <cleonicealvesdesouza09@gmail.com>
2022-08-02 10:21:41 -0300

    Faixa de preço

--- ex14/miniProjeto14.js
+++ ex14/miniProjeto14.js
@@ -1,10 +1,17 @@
-//Trocar os valores
-let a = 'vermelho';
-let b = 'azul';
-let aux;
+//Faixa de preço
+//Criar um array de objetos de faixa de preço para que ela possa ser usaddo em um site igual ao mercado livre.
 
-aux = a;
-a = b;
-b = aux;
-console.log(a, b);
+function criaFaixaPreco(tooltip, minimo,maximo) {
+    return {
+        tooltip,
+        minimo,
+        maximo
+    }
+}
 
+let faixas2 = [
+    criaFaixaPreco ("a",1,100),
+    criaFaixaPreco ("b",100,1000),
+    criaFaixaPreco ("c",1000,10000),
+]
+console.log(faixas2);
